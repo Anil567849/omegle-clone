330c22324c36ac803b9caf2970c68dcabe3ad02b
Anil Kumar Behera <anilkumar567849@gmail.com>
2024-08-07 11:31:48 +0530

    webrtc implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Landing from './pages/Landing';
-import Room from './pages/Room';
+import {Landing} from './pages/Landing';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/room" element={<Room />} />
     </Routes>
   );
 }
